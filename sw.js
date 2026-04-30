86c60869796600027e39abb1ced82eaef9bad812
m-ariza <m.ariza214@gmail.com>
2026-04-30 14:24:07 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,39 @@
-const CACHE_NAME = 'gammacalc-v2';
+const CACHE_NAME = 'gamma-calc-v1';
 const ASSETS_TO_CACHE =[
-  './',
-  './index.html',
-  './manifest.json'
+    './',
+    './index.html',
+    './manifest.json'
 ];
 
-// Install event: Cache the files
+// Install Event: Cache the local files
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-  );
-});
-
-// Fetch event: Serve from cache if offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// Activate event: Clean up old caches if you update the app
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS_TO_CACHE);
         })
-      );
-    })
-  );
+    );
+});
+
+// Fetch Event: Serve from cache first (fully offline), then try network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate Event: Cleanup older versions of caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
